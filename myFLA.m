fbde3e44eb72d6f6757a46a4e15a79d305db5873
Schnee1 <manuel.sonnleithner@gmail.com>
2017-04-28 17:42:37 +0200

    full code teted. Now need to test trading results.

--- myFLA.m
+++ myFLA.m
@@ -4,7 +4,7 @@ clear global; % clear all global variables from previous runs
     
     %% INPUT PARAMETER
 % Instrument        = which data to load, e.g. 'EURUSD'
-% totalDataSize   = measured from the first data point, how much data
+% totalDataSize     = measured from the first data point, how much data
 %                   should be taken into calculation for the consecutive walks.
 %                   FLA stops when end of totalWindowSize is reached
 % windowLenght_iS   = window length of data for in-sample optimization, measured in trading days, default = 518 
@@ -13,6 +13,15 @@ clear global; % clear all global variables from previous runs
 % if graphics       == 1 plotting is activated
 %--------------------------------------------------------------------------
 
+%% DATA PREPARATION
+
+% read price data and dates
+[data txt] = xlsread(Instrument);
+
+% convert txt-dates to datetime variables
+ dates = datetime(txt(2:end,1));
+%----------------------------------------------------------------------
+    
 %% CHECK FOR CORRECT INPUT
 
 if graphics > 1
@@ -27,6 +36,11 @@ elseif (windowLenght_iS + windowLength_ooS) > totalDataSize
 elseif moveInterval > totalDataSize
     error_moveinterval = 'Please check your input parameters, moveInterval must not exceed totalDataSize'
     
+    
+% elseif totalDataSize > size(data) - windowLenght_iS - windowLength_ooS
+%     error_totalDataSize = 'Please check your input parameters, totalDataSize is greater than available data in the price-data file'
+%     availableData = (length(data) - windowLenght_iS - windowLength_ooS)
+    
 else %no input error -> run code   
     
     %% INITIALIZE VECTORS/ARRAYS/VARIABLES
@@ -34,25 +48,10 @@ else %no input error -> run code
     % global to be available in all functions        
     global initBalance;    
     
-    count_walks = 0; % count how many forward-walks are performed
-    
-%     startDate_iS = datetime(50,1); % prepare array for 50 walks
-%     startDate_ooS = datetime(50,1); % prepare array for 50 walks
-%     endDate_iS = zeros(50,1); % prepare array for 50 walks
-%     endDate_ooS = zeros(50,1); % prepare array for 50 walks
-    
+    count_walks = 0; % count how many forward-walks are performed    
     flaEquity = 0; % array with all generated PL
     initBalance = 10000; % Initial account balance
     
-    %----------------------------------------------------------------------
-    
-    %% DATA PREPARATION
-    
-    % read price data and dates
-    [data txt] = xlsread(Instrument);
-    
-    % convert txt-dates to datetime variables
-     dates = datetime(txt(2:end,1));
     %----------------------------------------------------------------------
     
     %% WALK FORWARD
@@ -66,27 +65,45 @@ else %no input error -> run code
         
         count_walks = count_walks + 1;
         
+        % ---------------------
+        % DEBUGGING
+        if count_walks == 2            
+            x = 0;            
+        end
+        % ---------------------
+        
         % Save start and end-dates for later print to command window
         startDate_iS(count_walks,:) = dates(date); 
         endDate_iS(count_walks,:) = dates(date + windowLenght_iS);
-        startDate_ooS(count_walks,:) = dates(date + windowLenght_iS);
+        startDate_ooS(count_walks,:) = dates(date + windowLenght_iS + 1);
         endDate_ooS(count_walks,:) = dates(date + windowLenght_iS + 1 + windowLength_ooS);
     
+        % Define the dataset for all further calculations
         data_iS = data(date : date + windowLenght_iS, :);
         data_ooS = data(date + windowLenght_iS + 1 : date + windowLenght_iS + 1 + windowLength_ooS, :);
         
+        % =================================================================
         % run in-sample optimization
         [optParam1, optParam2] = runOptimizer_iS(5, 15, 1, 5, 1, 1, data_iS);
         
         % run out-of-sample backtest
         [ooS_pdRatio, ooS_equity] = runBacktest_ooS(optParam1, optParam2, data_ooS);       
+        % =================================================================
         
+        % Save the results in vectors for later display in command window                     
+        optParams(count_walks,:) = [optParam1, optParam2];
+        ooS_pdRatios(count_walks,:) = ooS_pdRatio;        
+        
+        % Calculate the combined forwardLooking-Equity by combining each out-of-sample equity curve
         if flaEquity(1) ~= 0 % only relevant in first walk
+           
             flaEquity = vertcat(flaEquity, ooS_equity); % merge new ooS_pl into final pl-array
-        else
-            flaEquity = ooS_equity; % only relevant in first walk
-        end
         
+        else
+            
+            flaEquity = ooS_equity; % only relevant in first walk
+       
+        end        
     end
     
     % Print to command window for controlling dates
@@ -95,12 +112,17 @@ else %no input error -> run code
     endDate_iS
     startDate_ooS
     endDate_ooS
-   
-    %======================================================================
-        
+    optParams
+    ooS_pdRatios   
+    
+    % Plot the combined FLA-Equity curve
     if (graphics == 1)
+        
         plot(flaEquity); % flaEquity = each ooS_equity combined
+        
     end
+    
+%---------------------------------------------------------------------------
     
 end
 
@@ -109,14 +131,10 @@ end
 function [optParam1, optParam2] = runOptimizer_iS(lowerLimit1, upperLimit1, lowerLimit2, upperLimit2, stepParam1, stepParam2, data)
 %% INPUT PARAMETER
 
-% lowerLimit1 = lower limit for optimization of parameter 1
-% upperLimit1 = upper limit for optimization of parameter 1
-% lowerLimit2 = lower limit for optimization of parameter 2
-% upperLimit2 = upper limit for optimization of parameter 2
-% stepParam1 = step forward for parameter 1 optimization
-% stepParam2 = step forward for parameter 2 optimization
-% param1 = input parameter 1 for the trading strategy
-% param2 = input parameter 2 for the trading strategy
+% lowerLimit1, lowerLimit2 = lower limit for optimization of parameter 1 / parameter 2
+% upperLimit1, upperLimit2 = upper limit for optimization of parameter 1 / parameter 2
+% stepParam1, setpParam2   = step forward interval for optimizing parameter 1 / parameter 2
+% param1, param2           = input parameter 1 / parameter 2 for the trading strategy
 
 % Initialize arrays with dimensions according to input limits
 iS_pdRatio = zeros(upperLimit1, upperLimit2- lowerLimit2+1);
@@ -291,21 +309,23 @@ for kk = param1+1 : length(data) % cicle through all candles of current data
             exitShort(kk);
             enterLong(kk);
         end
+   
         
-    else % ADD DISPLAY FOR ALL OPTIM PARAMS
+    % NO CROSSING OCCURED - NO CHANGES IN A RUNNING TRADE    
+    else 
         
-        if (runningTrade(kk-1) == -1) % if we have a running short trade set running trade to 1 for each bar until exit
-        runningTrade(kk) = -1;
-        
-        
-        elseif (runningTrade(kk-1) == 1) % if we have a running long trade set running trade to 1 for each bar until exit 
-        runningTrade(kk) = 1;
+        if (runningTrade(kk-1) == -1) % if yesterday = running short trade -> today = running short trade
+            runningTrade(kk) = -1;
         
         
-        elseif (runningTrade(kk-1) == 0) % if we have a running long trade set running trade to 1 for each bar until exit 
-        runningTrade(kk) = 0;
-        end
-    
+        elseif (runningTrade(kk-1) == 1) % if yesterday = running long trade -> today = running long trade
+            runningTrade(kk) = 1;
+        
+        
+        elseif (runningTrade(kk-1) == 0) % if yesterday = no trade -> today = no trade
+            runningTrade(kk) = 0;
+        
+        end    
     end    
 end
 
@@ -313,30 +333,38 @@ end
 
 totalPL = sum(profitLoss); 
 
-% clean profitLoss array from zeros
-cleanPL = profitLoss;
-cleanPL(find(cleanPL == 0)) = [];
+% Clean profitLoss array from zeros
+cleanPL = profitLoss; % use new array for further changes -> profitLoss array should not be changed
+cleanPL(find(cleanPL == 0)) = []; % delete value if value = 0
 
-% calculate cleanEquity curve with clean profitLoss array
-cleanEquity(1) = initBalance;
+% Calculate cleanEquity curve with clean profitLoss array
+cleanEquity(1) = initBalance; % first vaule = initial account balance
+
 for kk = 2:length(cleanPL)
+    
+        % calculate equity curve by adding up each profit/loss to current account balance
         cleanEquity(kk,1) = cleanEquity(kk-1) + cleanPL(kk); %[€]        
+
 end
 
-% plot(cleanEquity); % only for testing and checking each equity graph
-
+% Calculate maximum drawdown of the equity-curve - use internal matlab function maxdrawdown()
 maxDrawdown = maxdrawdown(cleanEquity);
 
-%ProfitDrawdownRatio
-pdRatio = totalPL / (maxDrawdown * 100);
+% Calculate ProfitDrawdownRatio
+pdRatio = totalPL / maxDrawdown;
+
+% Plot the equity curve - only for testing and control of each equity graph
+% plot(cleanEquity);  
 
 end
 
-% =========================================================================
 
-% DEFAULT INPUT
+%% DEFAULT INPUT
+
+% =========================================================================
 % myFLA(Instrument, totalWindowSize, windowLenght_iS, windowLength_ooS, moveInterval, graphics)
 % myFLA('EURUSD', 2000, 518, 259, 259, 0)
+% =========================================================================
 
 %% FRAGEN:
 
