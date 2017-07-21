9f7b5c3fed609c070b0bfe2128a0e8e28b4128b2
Schnee1 <manuel.sonnleithner@gmail.com>
2017-07-21 11:32:56 +0200

    change matrix logic = change3) of FLA_Comments.docx.
    235-246 changed

--- myFLA.m
+++ myFLA.m
@@ -230,28 +230,29 @@ function [optParam1, optParam2] = runOptimizer_iS(lowerLimit1, upperLimit1, step
         % -- Param2 = Multiplier
 
     % Preallocate array with dimensions according to input limits
-    iS_pdRatio = NaN(upperLimit1 / stepParam1, upperLimit2 / stepParam2);
+    iS_pdRatio = NaN((upperLimit1 - lowerLimit1)/ stepParam1 + 1, (upperLimit2 - lowerLimit2) / stepParam2 + 1);
 
     % cicle through all parameter combinations and create a heatmap
-    for ii = (lowerLimit1 / stepParam1) : (upperLimit1 / stepParam1) % cicle through each column = ATR, divisions necc. for steps < 1
-        for jj = (lowerLimit2 / stepParam2) : (upperLimit2 / stepParam2) % cicle through each row = Multiplier, divisions necc. for steps < 1
+    for ii = 1 : (upperLimit1 - lowerLimit1)/ stepParam1 + 1 % cicle through each column = ATR, divisions necc. for steps < 1
+        for jj = 1 : (upperLimit2 - lowerLimit2) / stepParam2 + 1 % cicle through each row = Multiplier, divisions necc. for steps < 1
 
-            currATR = ii * stepParam1; % convert back to use as input param for trading 
-            currMult = jj * stepParam2; % convert back to use as input param for trading
+            currATR = lowerLimit1 + (ii * stepParam1) - stepParam1; % convert back to use as input param for trading 
+            currMult = lowerLimit2 + (jj * stepParam2) - stepParam2; % convert back to use as input param for trading
 
-            % trade on current data set with ii and jj as input parameter, pd_ratio is returned and saved for each walk       
+            % trade on current data set with ii and jj as input parameter, pd_ratio is returned and saved for each walk
             pdRatio = trade_strategy(currATR, currMult, data); 
             iS_pdRatio(ii,jj) = pdRatio; % save result in array
 
         end
+        
     end
 
     % detect max value of pdRatio-array and save the position-indices of it -> optimal input parameter
     [maximumTemp, indexTemp] = max(iS_pdRatio); % detect max of each column and save as vector
     [max_, ind] = max(maximumTemp); % detect max of the above saved maximum-vector
 
-    optParam1 = indexTemp(ind) * stepParam1;
-    optParam2 = ind * stepParam2;
+    optParam1 = indexTemp(ind) * stepParam1 + lowerLimit1 - stepParam1; % convert from index to real trade-strategy input
+    optParam2 = ind * stepParam2 + lowerLimit2 - stepParam2; % convert from index to real trade-strategy input
 
 end
 
