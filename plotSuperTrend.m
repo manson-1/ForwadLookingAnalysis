b2c5b1d44eaf55b6ad076ef87c421eb8dbacc4cb
Schnee1 <manuel.sonnleithner@gmail.com>
2017-07-26 19:50:13 +0200

    backup files (.asv) and added plotSuperTrend function mainly for debugging

---
+++ plotSuperTrend.m
@@ -0,0 +1,9 @@
+function [] = plotSuperTrend(open, high, low, close, supertrend)   
+figure;
+         s1 = subplot(1,1,1);
+                candle(high,low,close,open,'k')
+                ylabel(s1, 'Price');
+                xlabel(s1, 'Date');
+                hold on
+                   plot(supertrend, 'b'); 
+                hold off
